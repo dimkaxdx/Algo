f13032f82ee91a9005ed7aacbc8b409e21ce35aa
dimkaxdx <dimkaxdx@gmail.com>
2017-11-18 17:06:50 +0300

    Tree view

--- Algo/Algo/__init__.py
+++ Algo/Algo/__init__.py
@@ -1,8 +1,41 @@
 from flask import *
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join, expanduser, basename
 
 app = Flask(__name__)
+
+def make_tree(path):
+    tree = dict(name=[path, basename(path)], children=[])
+    try: lst = listdir(path)
+    except OSError:
+        pass
+    else:
+        for name in lst:
+            fn = join(path, name)
+            if isdir(fn):
+                tree['children'].append(make_tree(fn))
+            else:
+                tree['children'].append(dict(name=[fn, name]))
+    return tree
+
+# def make_tree(path):
+#     tree = dict(name=os.path.basename(path), children=[])
+#     try: lst = os.listdir(path)
+#     except OSError:
+#         pass #ignore errors
+#     else:
+#         for name in lst:
+#             fn = os.path.join(path, name)
+#             if os.path.isdir(fn):
+#                 tree['children'].append(make_tree(fn))
+#             else:
+#                 tree['children'].append(dict(name=name))
+#     return tree
+
+@app.route('/tree')
+def dirtree():
+    path = expanduser(u'./static/Algo_source')
+    return render_template('dirtree.html', tree=make_tree(path))
 
 @app.errorhandler(404)
 def page_not_found(e):
